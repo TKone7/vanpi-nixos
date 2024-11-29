ea65e0ba3682d6d2c7cdf7bd01ee7e8fad1e8681
TKone7 <tobias.koller@protonmail.com>
2024-11-29 09:19:21 +0100

    rename switches to their respective order

--- components/home-assistant/default.nix
+++ components/home-assistant/default.nix
@@ -40,14 +40,14 @@
           platform = "mcp23017";
           i2c_address = 32; # TODO this is actually the hex address "0x20". maybe there is a way to convert in Nix lang
           pins = {
-            "8" = "switch_8";
-            "9" = "switch_9";
-            "10" = "switch_10";
-            "11" = "switch_11";
-            "12" = "switch_12";
-            "13" = "switch_13";
-            "14" = "switch_14";
-            "15" = "switch_15";
+            "8" = "switch_1";
+            "9" = "switch_2";
+            "10" = "switch_3";
+            "11" = "switch_4";
+            "12" = "switch_5";
+            "13" = "switch_6";
+            "14" = "switch_7";
+            "15" = "switch_8";
           };
         }
         #{
